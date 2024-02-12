10be2873d0484765129591bc547d1fd5bce8188a
Pradeep Tammali <pradeepkumartammali@gmail.com>
2024-02-12 16:22:58 +0100

    chore: cli for removing test databases

--- cli/development/main.py
+++ cli/development/main.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 import click
+from sqlalchemy import inspect, make_url
+from sqlalchemy_utils import database_exists, drop_database
+
+from omdb.config import config
+from omdb.db.base import db
 
 
 @click.group(name='dev')
@@ -20,4 +25,29 @@ def server(context, host: str, port: int, debug: bool):
     context.obj.app.run(host=host, port=port, debug=debug)
 
 
+@click.command()
+@click.option('--remove_all', default=False, type=bool)
+@click.pass_context
+def clean_db(context, remove_all: bool):
+    """Clean up databases"""
+
+    if not config.is_development():
+        raise click.ClickException('This command is only available in development mode.')
+
+    with context.obj.app.app_context():
+        _inspect = inspect(db.engine)
+        for db_name in _inspect.get_schema_names():
+            if not remove_all and not db_name.startswith('test'):
+                click.secho(f'Skipping database: {db_name}', fg='yellow')
+                continue
+
+            db_url = make_url(config.SQLALCHEMY_DATABASE_URI)
+            db_url = db_url.set(database=f'{db_name}')
+            db_uri = db_url.render_as_string(hide_password=False)
+            if database_exists(db_uri):
+                click.secho(f'Dropping database: {db_name}', fg='red', bold=True)
+                drop_database(db_uri)
+
+
 subcommands.add_command(server)
+subcommands.add_command(clean_db)
